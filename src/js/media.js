bb8a9447702b2257b1ad41d6a45302b8cf611dff
jlyneu <jly@ccs.neu.edu>
2015-11-19 04:07:03 -0500

    Refactoring and adding comments, adding song menu functionality

--- src/js/media.js
+++ src/js/media.js
@@ -1,73 +1,106 @@
 /**
- * Set of GIF dancer names and giphy ids
+ * Contains variables and functions used to retrieve images, GIFs, and music
+ * for the GIF Dance Party
  */
-var gifDancers = {
-    carlton: 'wn8rVP7qC8TNC',
-    pumpGirl: '26BkNiQlYeHc5jpf2'
-};
-
-var gdpPlaylist = [
-    'bazz', 'bubblebutt', 'fatboy', 'grounded',
-    'halffull', 'nahnahnah', 'singalong', 'trololo',
-    'twist', 'walkmen', 'wegotyou'
-];
-
-var thumbnailImgurIds = {
-    '8bitcarlton': 'ifuoPqn',
-    apple: 'wbv3RCd',
-    arnold: 'Sh6WPJs',
-    babe: 'hDWrKI7',
-    banana: 'Li3pOWH',
-    bananabounce: 'Ky3jiGQ',
-    bear: '7KW1zWk',
-    blob: 'P7meKeB',
-    bmo: 'Q6Jyks5',
-    bonebreak: 'otdi7m9',
-    bones: 'WEl9Npv',
-    breakglitch: 'TXYwR2F',
-    brian: 'mcVtWQT',
-    bunny: 'OPfS7wP',
-    bunnyspank: 'G9Bs0N9',
-    business: 'QaSTmdL',
-    cact: 'fn0xLOv',
-    carlton: 'uF179oM',
-    charlie: 'aXXDfwI',
-    chicken: 'Uckh2i8',
-    cowboy: 'sCxQ7om',
-    discoball: 'FqqZgIZ',
-    doggy: 'Q6r1gYp',
-    elvenbooty: '1GZgAQV',
-    fabio: '320x7kR',
-    fatspidey: '5GZYEmt',
-    foreveralone: 'jgOvt9T',
-    fred: 'FSvV7is',
-    fuzzball: 'ehDJ5bp',
-    gene: 'nfO1s5a'
+gdpMedia =  {
+    /* List of GIF Dance Party song titles */
+    gdpPlaylist: [
+        'bazz', 'bubblebutt', 'fatboy', 'grounded',
+        'halffull', 'nahnahnah', 'singalong', 'trololo',
+        'twist', 'walkmen', 'wegotyou'
+    ],
+    /* Mapping from dancer names to Giphy ids for dancer GIFs */
+    giphyIds: {
+        carlton: 'wn8rVP7qC8TNC',
+        pumpGirl: '26BkNiQlYeHc5jpf2'
+    },
+    /* Mapping from dancer names to Imgur ids for dancer thumbnails */
+    imgurIds: {
+        '8bitcarlton': 'ifuoPqn',
+        apple: 'wbv3RCd',
+        arnold: 'Sh6WPJs',
+        babe: 'hDWrKI7',
+        banana: 'Li3pOWH',
+        bananabounce: 'Ky3jiGQ',
+        bear: '7KW1zWk',
+        blob: 'P7meKeB',
+        bmo: 'Q6Jyks5',
+        bonebreak: 'otdi7m9',
+        bones: 'WEl9Npv',
+        breakglitch: 'TXYwR2F',
+        brian: 'mcVtWQT',
+        bunny: 'OPfS7wP',
+        bunnyspank: 'G9Bs0N9',
+        business: 'QaSTmdL',
+        cact: 'fn0xLOv',
+        carlton: 'uF179oM',
+        charlie: 'aXXDfwI',
+        chicken: 'Uckh2i8',
+        cowboy: 'sCxQ7om',
+        discoball: 'FqqZgIZ',
+        doggy: 'Q6r1gYp',
+        elvenbooty: '1GZgAQV',
+        fabio: '320x7kR',
+        fatspidey: '5GZYEmt',
+        foreveralone: 'jgOvt9T',
+        fred: 'FSvV7is',
+        fuzzball: 'ehDJ5bp',
+        gene: 'nfO1s5a'
+    },
+    /* List of GIF Dance Party dancer names */
+    dancerNameList: [
+        '8bitcarlton', 'apple', 'arnold', 'babe',
+        'banana', 'bananabounce', 'bear', 'blob',
+        'bmo', 'bonebreak', 'bones', 'breakglitch',
+        'brian', 'bunny', 'bunnyspank', 'business',
+        'cact', 'carlton', 'charlie', 'chicken',
+        'cowboy', 'discoball', 'doggy', 'elvenbooty',
+        'fabio', 'fatspidey', 'foreveralone', 'fred',
+        'fuzzball', 'gene', 'ghosty', 'girlstep',
+        'glitchtwist', 'greenlady', 'greenlady2', 'greything',
+        'headspin', 'hellicopter', 'hipdog', 'hotdogs',
+        'hula', 'humpery', 'jason', 'kirby',
+        'kitty', 'lilguy', 'maranda', 'matrix',
+        'mj', 'momdance', 'morpher', 'nerd',
+        'noshorts', 'nudeflames', 'obama', 'orangeguy',
+        'pa', 'patrick', 'pedopickle', 'pilboy',
+        'pixelly', 'pizza', 'ponyshuffle', 'possum',
+        'pretzel', 'psych', 'pumpgirl', 'ravecat',
+        'robothump', 'roger', 'runningworm', 'saxguy',
+        'scorpion', 'shawty', 'slappy', 'smooch',
+        'speakerhead', 'spinglitch', 'spongeybob', 'squidword',
+        'stripper1', 'stripper2', 'tej', 'thehop',
+        'thewizard', 'theworm', 'tim', 'twerk',
+        'twerk2', 'whiteguy', 'wormlady', 'yeti'
+    ],
+    /* Retrieve the Imgur url for the given dancer's thumbnail image */
+    getThumbnailUrl: function(dancerName) {
+        return "https://imgur.com/" + gdpMedia.imgurIds[dancerName] + ".png";
+    },
+    /* Retrieve the Giphy url for the given dancer's GIF */
+    getGifUrl: function(dancerName) {
+        return "https://media.giphy.com/media/" + gdpMedia.giphyIds[dancerName] + "/giphy.gif";
+    },
+    /* Send a message to the background script to play the song with the
+       given name */
+    selectSong: function(name) {
+        chrome.runtime.sendMessage({
+            type: "songName",
+            songName: name
+        });
+    },
+    /* Send a message to the background script to stop playing audio */
+    stopAudio: function() {
+        chrome.runtime.sendMessage({
+            type: "stopAudio"
+        });
+    },
+    /* When leaving the current page, send a message to the background script
+       to stop playing audio */
+    unloadAudio: function() {
+        // when leaving the page, be sure to stop the gdp audio
+        $(window).unload(function() {
+            gdpMedia.stopAudio();
+        });
+    }
 }
-
-/*
-var dancerNameList = [
-    '8bitcarlton', 'apple', 'arnold', 'babe',
-    'banana', 'bananabounce', 'bear', 'blob',
-    'bmo', 'bonebreak', 'bones', 'breakglitch',
-    'brian', 'bunny', 'bunnyspank', 'business',
-    'cact', 'carlton', 'charlie', 'chicken',
-    'cowboy', 'discoball', 'doggy', 'elvenbooty',
-    'fabio', 'fatspidey', 'foreveralone', 'fred',
-    'fuzzball', 'gene', 'ghosty', 'girlstep',
-    'glitchtwist', 'greenlady', 'greenlady2', 'greything',
-    'headspin', 'hellicopter', 'hipdog', 'hotdogs',
-    'hula', 'humpery', 'jason', 'kirby',
-    'kitty', 'lilguy', 'maranda', 'matrix',
-    'mj', 'momdance', 'morpher', 'nerd',
-    'noshorts', 'nudeflames', 'obama', 'orangeguy',
-    'pa', 'patrick', 'pedopickle', 'pilboy',
-    'pixelly', 'pizza', 'ponyshuffle', 'possum',
-    'pretzel', 'psych', 'pumpgirl', 'ravecat',
-    'robothump', 'roger', 'runningworm', 'saxguy',
-    'scorpion', 'shawty', 'slappy', 'smooch',
-    'speakerhead', 'spinglitch', 'spongeybob', 'squidword',
-    'stripper1', 'stripper2', 'tej', 'thehop',
-    'thewizard', 'theworm', 'tim', 'twerk',
-    'twerk2', 'whiteguy', 'wormlady', 'yeti'
-];*/
--- src/js/script.js
+++ src/js/script.js
@@ -1,63 +1,21 @@
+/******************************************************************************
+ * CONSTANTS
+ *****************************************************************************/
+
+/** Maximum z-index value */
+var maxZIndex = Math.pow(2, 31);
+
 /** z-index of front-most GIF */
-var frontZIndex = Math.pow(2, 31) - 500;
+var frontZIndex = maxZIndex - 500;
 
 /** z-index of back-most GIF */
 var backZIndex = frontZIndex - 1;
 
-//listen for the button click to start and stop
-chrome.runtime.onMessage.addListener(
-    function(request, sender) {
-        if(request.type == "startParty"){
-            startTheParty();
-        }
-        else if(request.type == "stopParty"){
-            stopTheParty();
-        }
-    });
+/******************************************************************************
+ * DANCER CREATION
+ *****************************************************************************/
 
-function getImgurThumbnailUrl(dancerName) {
-    return "https://imgur.com/" + thumbnailImgurIds[dancerName];
-}
-
-function getPreviewGifUrl(dancerName) {
-    return "https://media2.giphy.com/media/" + gifDancers[dancerName] + "/200.gif";
-}
-
-function getFullSizeGifUrl(dancerName) {
-    return "https://media.giphy.com/media/" + gifDancers[dancerName] + "/giphy.gif";
-}
-
-function selectSong(name) {
-    chrome.runtime.sendMessage({
-        type: "songName",
-        songName: name
-    });
-}
-
-function stopAudio() {
-    chrome.runtime.sendMessage({
-        type: "stopAudio"
-    });
-}
-
-function thePartyIsOff() {
-    return $('.gdp-menu').length === 0;
-}
-
-function startTheParty() {
-    createMenu();
-    selectSong('singalong');
-    createGIFDancer('carlton');
-    $('.gdp-start').text("STOP THE PARTY");
-}
-
-function stopTheParty() {
-    stopAudio();
-    $('.gdp-menu').remove();
-    $('.gdp-dancer').remove();
-    $('.gdp-start').text("START THE PARTY");
-}
-
+/* Horizontally flip the given image element */
 function flipImage($image) {
     if ($image.hasClass('gdp-flipped')) {
         $image.removeClass('gdp-flipped');
@@ -66,43 +24,67 @@ function flipImage($image) {
     }
 }
 
+/* Create a GIF of the dancer with the given name in the top left corner
+   of the screen. On hover, the GIF will have an X button in the top left,
+   a FLIP, FRONT, and BACK button in the top right, a CLONE button in
+   the bottom left, and a RESIZE handle in the bottom right. GIFS should
+   by default be in front of the other divs upon creation. */
 function createGIFDancer(dancerName) {
-    var gifUrl = getFullSizeGifUrl(dancerName);
+    // Giphy url for the GIF dancer
+    var gifUrl = gdpMedia.getGifUrl(dancerName);
+    // Img element of the GIF dancer
+    var gifImg = $('<img src="' + gifUrl + '" />');
 
-    var gifImg = $('<img class="gdp-resizable" src="' + gifUrl + '" />');
-    var closeDiv = $('<div class="gdp-close gdp-opt gdp-top-left">X</div>')
-    .click(function() {
-        $(this).parent().remove();
-    });
-    var flipDiv = $('<div class="gdp-flip gdp-opt gdp-top-right">FLIP</div>')
-    .click(function() {
-        flipImage($(this).parent().find('img'));
-    });
-    var backDiv = $('<div class="gdp-back gdp-opt gdp-top-right">BACK</div>')
-    .click(function() {
-        $(this).parent().css('z-index', backZIndex);
-        backZIndex--;
-    });
-    var frontDiv = $('<div class="gdp-front gdp-opt gdp-top-right">FRONT</div>')
-    .click(function() {
-        $(this).parent().css('z-index', frontZIndex);
-        frontZIndex++;
-    });
-    var cloneDiv = $('<div class="gdp-clone gdp-opt gdp-bottom-left">CLONE</div>')
-    .click(function() {
-        createGIFDancer(dancerName);
-    });
-    var resizeDiv = $('<div class="gdp-resize gdp-opt gdp-bottom-right">RESIZE</div>');
+    // ----------------- create the various dancer options ----------------- //
+
+    // Remove the GIF dancer on click of the close X button
+    var closeDiv = $('<div class="gdp-opt gdp-top-left">X</div>')
+        .click(function() {
+            $(this).parent().remove();
+        });
+    // Horizontally flip the GIF dancer on click of the FLIP button
+    var flipDiv = $('<div class="gdp-opt gdp-top-right">FLIP</div>')
+        .click(function() {
+            flipImage($(this).parent().find('img'));
+        });
+    // Send the GIF dancer to the back on click of the BACK button
+    var backDiv = $('<div class="gdp-opt gdp-top-right">BACK</div>')
+        .click(function() {
+            $(this).parent().css('z-index', backZIndex);
+            backZIndex--;
+        });
+    // Send the GIF dancer to the front on click of the FRONT button
+    var frontDiv = $('<div class="gdp-opt gdp-top-right">FRONT</div>')
+        .click(function() {
+            $(this).parent().css('z-index', frontZIndex);
+            frontZIndex++;
+        });
+    // Create a new GIF of the same dancer on click of the CLONE button
+    var cloneDiv = $('<div class="gdp-opt gdp-bottom-left">CLONE</div>')
+        .click(function() {
+            createGIFDancer(dancerName);
+        });
+    // On mouse down on the RESIZE button, allow the user to resize the GIF
+    var resizeDiv = $('<div class="gdp-opt gdp-bottom-right">RESIZE</div>');
+
+    // --------------------------------------------------------------------- //
+
+    // create the dancer div containing the GIF and the various dancer options
     var dancerDiv = $('<div class="gdp-dancer"></div>')
+        // set the z-index such that this dancer is in front
         .css('z-index', frontZIndex)
+        // append the buttons for the dancer options
         .append(closeDiv)
         .append(flipDiv)
         .append(backDiv)
         .append(frontDiv)
         .append(cloneDiv)
         .append(resizeDiv)
+        // append the actual GIF dancer
         .append(gifImg)
+        // allow the user to drag this div around the screen
         .draggable()
+        // on hover, display the dancer options
         .hover(function() {
             $(this).css('background-color', 'rgba(0,0,0,.3)');
             $(this).find('.gdp-opt').css('color', 'rgba(255,255,255,1)');
@@ -110,66 +92,197 @@ function createGIFDancer(dancerName) {
             $(this).css('background-color', 'rgba(0,0,0,0)');
             $(this).find('.gdp-opt').css('color', 'rgba(0,0,0,0)');
         });
+
+    // increment the frontZIndex value so the next created GIF will be in front
     frontZIndex++;
+    // append the dancer to the screen
     $('body').append(dancerDiv);
 }
 
-function createMenu() {
+/******************************************************************************
+ * MENU CREATION
+ *****************************************************************************/
+
+/* Append the main menu containing "ADD DANCER" and "SELECT SONG" options
+   to the bottom right corner of the screen */
+function createMainMenu() {
+
     var menuHtml = [
-        '<div class="gdp-menu">',
+        '<div class="gdp-main-menu">',
             '<div class="gdp-add-dancer">ADD DANCER</div>',
             '<div class="gdp-select-song">SELECT SONG</div>',
         '</div>'
     ].join('');
+
+    // append the menu to the screen
     $('body').append(menuHtml);
+    $('.gdp-main-menu').css('z-index', maxZIndex);
+
+    // set a click listener for the "ADD DANCER" menu option
     $('.gdp-add-dancer').click(function() {
-        createDancerMenu();
-    })
+        createAddDancerMenu();
+    });
+    // set a click listener for the "SELECT SONG" menu option
+    $('.gdp-select-song').click(function() {
+        createSelectSongMenu();
+    });
 }
 
-function createDancerIcon(dancerName, imageUrl) {
+// ---------------------- create the ADD DANCER menu ----------------------- //
 
-    var dancerIconDiv = $('<div class="gdp-dancer-icon"></div>');
-    var dancerThumbnailImg = $('<img class="gdp-dancer-thumbnail" src="' + imageUrl + '.png" />');
-    dancerIconDiv.append(dancerThumbnailImg);
+/* Return an element representing the 100px by 100px thumbnail of the GIF
+   dancer framed by a white circle with a black border */
+function createThumbnail(dancerName, imageUrl) {
+
+    var dancerThumbnailWrapper = $('<div class="gdp-thumbnail-wrapper"></div>');
+    var dancerThumbnailImg = $('<img class="gdp-thumbnail-img" src="' + imageUrl + '.png" />');
+
+    // on click of the thumbnail, remove the ADD DANCER menu and append the
+    // corresponding dancer GIF to the top left corner of the screen
     dancerThumbnailImg.click(function() {
         createGIFDancer(dancerName);
-        $('.gdp-dancer-menu').remove();
+        $('.gdp-add-dancer-menu').remove();
     });
 
-    return dancerIconDiv;
+    dancerThumbnailWrapper.append(dancerThumbnailImg);
+    return dancerThumbnailWrapper;
 }
 
-function createDancerMenu() {
+/* Create the ADD DANCER menu, containing the thumbnails of all the GDP GIF
+   dancers. On click of a GIF dancer thumbnail, the menu will be removed and
+   the GIF dancer will be appended to the screen. Clicking on anything besides
+   the thumbnails will close the ADD DANCER menu without appending any GIF
+   dancers to the screen */
+function createAddDancerMenu() {
 
-    var dancerIconDivs = [];
+    // a list to hold the DOM elements of the GIF dancer thumbnails
+    var thumbnailDivs = [];
 
-    for (var dancerName in thumbnailImgurIds) {
-        if (thumbnailImgurIds.hasOwnProperty(dancerName)) {
-            var imgurUrl = getImgurThumbnailUrl(dancerName);
-            dancerIconDivs.push(createDancerIcon(dancerName, imgurUrl));
+    // loop through all of the GIF dancer names and add the thumbnail DOM
+    // element for the corresponding dancers to the thumbnailDivs list
+    for (var dancerName in gdpMedia.imgurIds) {
+        if (gdpMedia.imgurIds.hasOwnProperty(dancerName)) {
+            var imgurUrl = gdpMedia.getThumbnailUrl(dancerName);
+            thumbnailDivs.push(createThumbnail(dancerName, imgurUrl));
         }
     }
 
-    var gdpDancerMenuDiv = $('<div class="gdp-dancer-menu"></div>');
-    var gdpDancerListContainer = $('<div class="gdp-dancer-list-container"></div>');
-    for (var i = 0; i < dancerIconDivs.length; i++) {
-        gdpDancerListContainer.append(dancerIconDivs[i]);
+    // transparent full screen div that contains the list of thumbnails, but
+    // will close on click
+    var gdpAddDancerMenu = $('<div class="gdp-menu"></div>')
+        .css('z-index', maxZIndex)
+        .click(function() {
+            $(this).remove();
+        });
+
+    // div containing the GIF dancer thumbnails
+    var gdpAddDancerList = $('<div class="gdp-menu-list"></div>');
+    // loop through the GIF dancer thumbnail list and append each thumbnail
+    // to the menu list
+    for (var i = 0; i < thumbnailDivs.length; i++) {
+        gdpAddDancerList.append(thumbnailDivs[i]);
     }
-    gdpDancerMenuDiv.append(gdpDancerListContainer);
 
-    $('body').append(gdpDancerMenuDiv);
+    // append the thumbnail list to the screen
+    gdpAddDancerMenu.append(gdpAddDancerList);
+    $('body').append(gdpAddDancerMenu);
 }
 
-/**
- * Create the START THE PARTY button in the bottom left corner of the screen
- */
-function unloadAudio() {
-    // when leaving the page, be sure to stop the gdp audio
-    $(window).unload(function() {
-        stopAudio();
+// ------------------------------------------------------------------------- //
+
+// ---------------------- create the SELECT SONG menu ---------------------- //
+
+/* Return an element representing a song choice for the GDP */
+function createSongOption(songName) {
+    var songOption = $('<div class="gdp-song-option">' + songName.toUpperCase() + '</div>');
+    // on click of the song option, remove the SELECT SONG menu and play the
+    // corresponding audio
+    songOption.click(function() {
+        gdpMedia.selectSong(songName);
+        $('.gdp-select-song-menu').remove();
     });
+    return songOption;
 }
 
-// kick it all off once the page loads
-$(document).ready(unloadAudio);
+/* Create the SELECT SONG menu, containing a list of available GDP songs to
+   play in the background. On click of a song option, the menu will be removed
+   and the new song will be played. Clicking on anything besides the song
+   options will close the SELECT SONG menu without playing a new song */
+   function createSelectSongMenu() {
+
+       // a list to hold the DOM elements of the song options
+       var songOptions = [];
+
+       // loop through all of the GDP song names and add the song option
+       // DOM element to the songOptions list
+       for (var i = 0; i < gdpMedia.gdpPlaylist.length; i++) {
+           songOptions.push(createSongOption(gdpMedia.gdpPlaylist[i]));
+       }
+
+       // transparent full screen div that contains the list of song options,
+       // but will close on click
+       var gdpSelectSongMenu = $('<div class="gdp-menu"></div>')
+           .css('z-index', maxZIndex)
+           .click(function() {
+               $(this).remove();
+           });
+
+       // div containing the song options
+       var gdpSelectSongList = $('<div class="gdp-menu-list"></div>');
+       // loop through the song options list and append each song option
+       // to the menu list
+       for (var i = 0; i < songOptions.length; i++) {
+           gdpSelectSongList.append(songOptions[i]);
+       }
+
+       // append the song option list to the screen
+       gdpSelectSongMenu.append(gdpSelectSongList);
+       $('body').append(gdpSelectSongMenu);
+   }
+
+
+// ------------------------------------------------------------------------- //
+
+/******************************************************************************
+ * STARTING AND STOPPING THE PARTY
+ *****************************************************************************/
+
+/* Return true if the part is off, meaning the main menu is not currently
+   displayed on the screen. Otherwise, return false */
+function thePartyIsOff() {
+    return $('.gdp-main-menu').length === 0;
+}
+
+/* Append the main menu to the screen, start the music, and append the default
+   GIF dancers to the screen */
+function startTheParty() {
+    createMainMenu();
+    gdpMedia.selectSong('singalong');
+    createGIFDancer('carlton');
+}
+
+/* Remove the main menu, stop the music, and remove the GIF dancers, ADD
+   DANCER menu, and SELECT SONG menu */
+function stopTheParty() {
+    $('.gdp-main-menu').remove();
+    gdpMedia.stopAudio();
+    $('.gdp-dancer').remove();
+    $('.gdp-menu').remove();
+}
+
+/******************************************************************************
+ * LISTENERS
+ *****************************************************************************/
+
+/* Set a listener for messages requesting to start and stop the GDP */
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if(request.type == "startParty"){
+        startTheParty();
+    }
+    else if(request.type == "stopParty"){
+        stopTheParty();
+    }
+});
+
+/* Set the listener to stop playing audio after leaving the current page */
+$(document).ready(gdpMedia.unloadAudio);
